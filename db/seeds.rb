70a8be3e211c37fae74b06bc13b905e3ea7f8f48
asraavisena <asra.avisena@yahoo.com>
2020-11-27 13:34:42 +0100

    seed changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Job.delete_all
+Joblanguage.delete_all
+# Language.delete_all
+# User.delete_all
 
-User.create(email: 'test2@test2.de',
-            password: '123test')
+# User.create(email: 'test2@test2.de',
+#             password: '123test')
+
+# Language.create(name: 'deutsch')
+# Language.create(name: 'englisch')
+# Language.create(name: 'indonesisch')
 
 Job.create(title: 'seed jobs 1',
-           salary: 20)
+           salary: 20, language_ids: [16, 17])
 Job.create(title: 'jobs 2',
-           salary: 5)
+           salary: 5, language_ids: [17, 18])
 Job.create(title: 'seed 3',
-           salary: 10)
+           salary: 10, language_ids: [16])
 Job.create(title: 'seed jobs 4',
-           salary: 8)
-
-Language.create(name: 'deutsch')
-Language.create(name: 'englisch')
-Language.create(name: 'indonesisch')
+           salary: 8, language_ids: [16, 17, 18])
