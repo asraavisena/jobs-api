1a5c7022c84ee5b7b3c6d232a387bb83f2ec859e
asraavisena <asra.avisena@yahoo.com>
2020-12-02 08:56:31 +0100

    adding user id to jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_080841) do
+ActiveRecord::Schema.define(version: 2020_12_02_075544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_080841) do
     t.integer "salary"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "languages", force: :cascade do |t|
